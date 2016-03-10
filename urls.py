af988d98c1169a2794a7cba1f32d74874d75eff0
Frank Schneider <info@shoul.de>
2016-03-10 11:57:06 +0100

    Move results as to the home url address

--- urls.py
+++ urls.py
@@ -4,10 +4,10 @@ from .views import (DiseaseListView, DiseaseDetailView, ResultIndex,
     ResultDetail)
 
 urlpatterns = [
-    url(r'^befunde/$', ResultIndex.as_view(), name='result_index'),
-    url(r'^befund/(?P<slug>[-\w]+)/$', ResultDetail.as_view(),
-        name='result_detail'),
-    url(r'^(?P<slug>[-\w]+)/$', DiseaseDetailView.as_view(),
+    url(r'^krankheitsmuster/(?P<slug>[-\w]+)/$', DiseaseDetailView.as_view(),
         name='disease_detail'),
-    url(r'^$', DiseaseListView.as_view(), name='diesase_index'),
+    url(r'^krankheitsmuster/$', DiseaseListView.as_view(), name='disease_index'),
+    url(r'^(?P<slug>[-\w]+)/$', ResultDetail.as_view(),
+        name='result_detail'),
+    url(r'^$', ResultIndex.as_view(), name='result_index'),
 ]
