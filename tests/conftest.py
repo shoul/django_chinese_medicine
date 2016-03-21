466c2a302baa1668adce267b90a56bd9c9ba5885
Frank Schneider <fs@shoul.de>
2016-03-21 15:15:42 +0100

    Rename model 'Result' to 'Symptom'.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 from pytest_factoryboy import register
 
-from django_chinese_medicine.factories import (ResultFactory, EtiologieFactory,
+from django_chinese_medicine.factories import (SymptomFactory, EtiologieFactory,
         DiseasePatternFactory, TherapyFactory)
 
-register(ResultFactory)
+register(SymptomFactory)
 register(EtiologieFactory)
 register(DiseasePatternFactory)
 register(TherapyFactory)
--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
-from .views import (DiseaseListView, DiseaseDetailView, ResultIndex,
-    ResultDetail, TherapyDetail)
+from .views import (DiseaseListView, DiseaseDetailView, SymptomIndex,
+    SymptomDetail, TherapyDetail)
 
 urlpatterns = [
     url(r'^krankheitsmuster/(?P<slug>[-\w]+)/$', DiseaseDetailView.as_view(),
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^krankheitsmuster/$', DiseaseListView.as_view(), name='disease_index'),
     url(r'^therapie/(?P<slug>[-\w]+)/$', TherapyDetail.as_view(),
     name='therapy_detail'),
-    url(r'^(?P<slug>[-\w]+)/$', ResultDetail.as_view(),
-        name='result_detail'),
-    url(r'^$', ResultIndex.as_view(), name='result_index'),
+    url(r'^(?P<slug>[-\w]+)/$', SymptomDetail.as_view(),
+        name='symptom_detail'),
+    url(r'^$', SymptomIndex.as_view(), name='symptom_index'),
 ]
